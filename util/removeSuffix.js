87f1601a3e557f3351b954ca06be573d57d39b1b
Jerry Jäppinen <eiskis@gmail.com>
2022-09-03 17:27:14 +0200

    Replaced .substr

--- util/removeSuffix.js
+++ util/removeSuffix.js
@@ -1,16 +1,12 @@
 import isSuffixed from './isSuffixed'
 
-const removeCharacters = (string, length) => {
-  return string.substr(0, string.length - length)
-}
-
 export default (string, suffix) => {
   if (string && string.length && suffix && suffix.length) {
     let result = '' + string
 
     if (suffix.length) {
       while (isSuffixed(result, suffix)) {
-        result = removeCharacters(result, suffix.length)
+        result = result.substring(0, result.length - suffix.length)
       }
     }
 
