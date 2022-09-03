86f4531d897bd7f13cf4600fb350cb2f9bbe4c08
Jerry Jäppinen <eiskis@gmail.com>
2022-09-03 17:26:12 +0200

    Replaced .substr

--- util/getDomain.js
+++ util/getDomain.js
@@ -3,10 +3,10 @@ export default (string) => {
     const urlParts = /^(?:\w+:\/\/)?([^/]+)(.*)$/.exec(string)
 
     if (urlParts && urlParts[1]) {
-      const sub = urlParts[1].substr(0, 4)
+      const sub = urlParts[1].substring(0, 4)
 
       if (sub === 'www.') {
-        return urlParts[1].substr(4)
+        return urlParts[1].substring(4)
       }
 
       return urlParts[1]
