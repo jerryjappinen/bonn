fcbb830ee9e0def39de15b764701699950a658a3
Jerry Jäppinen <eiskis@gmail.com>
2022-10-01 15:37:14 +0200

    Removed index files

--- stores/hygraph.js
+++ stores/hygraph.js
@@ -1,5 +1,9 @@
 import { defineStore } from 'pinia'
 
+import { request } from 'graphql-request'
+
 import hygraph from './setup/hygraph'
 
-export default defineStore('hygraph', hygraph())
+export default defineStore('hygraph', hygraph({
+  request
+}))
