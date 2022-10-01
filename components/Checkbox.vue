b7a898b5c6b5049071f7376bb95d152d3f59bd19
Jerry Jäppinen <eiskis@gmail.com>
2022-10-01 15:10:35 +0200

    Checkbox

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,140 +1,140 @@
 <script setup>
-// This is a read-only component that visualizes state
-// Wrap this component in a Toggle to deliver complete form element behavior
-const props = defineProps({
+  // This is a read-only component that visualizes state
+  // Wrap this component in a Toggle to deliver complete form element behavior
+  const props = defineProps({
 
-  modelValue: {
-    type: Boolean,
-    default: false
-  },
+    modelValue: {
+      type: Boolean,
+      default: false
+    },
 
-  disabled: {
-    type: Boolean,
-    default: false
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+
+  })
+  </script>
+
+  <template>
+    <span
+      class="c-checkbox"
+      :class="{
+        'c-checkbox-on': !!modelValue,
+        'c-checkbox-off': !modelValue,
+        'c-checkbox-disabled': disabled,
+        'c-checkbox-enabled': !disabled
+      }"
+    >
+      <slot>
+        <IconCheck
+          class="c-checkbox-icon"
+          :class="{
+            'c-checkbox-icon-on': !!modelValue,
+            'c-checkbox-icon-off': !modelValue,
+            'c-checkbox-icon-disabled': disabled,
+            'c-checkbox-icon-enabled': !disabled
+          }"
+        />
+      </slot>
+    </span>
+  </template>
+
+  <style lang="scss">
+
+  // NOTE: this could also be confined in component class name
+  // But then overriding it will be more laborious
+  :root {
+    --c-checkbox-width: 1em;
+    --c-checkbox-border-width: 2px;
+    --c-checkbox-radius: var(--round);
+
+    --c-checkbox-enabled-on-color: var(--inverted-offset-background-color);
+    --c-checkbox-enabled-off-color: var(--text-color);
+    --c-checkbox-enabled-check-color: var(--inverted-text-color);
+
+    --c-checkbox-disabled-on-color: var(--discreet-color-very-light);
+    --c-checkbox-disabled-off-color: var(--discreet-text-color);
+    --c-checkbox-disabled-check-color: var(--discreet-text-color);
   }
 
-})
-</script>
-
-<template>
-  <span
-    class="c-checkbox"
-    :class="{
-      'c-checkbox-on': !!modelValue,
-      'c-checkbox-off': !modelValue,
-      'c-checkbox-disabled': disabled,
-      'c-checkbox-enabled': !disabled
-    }"
-  >
-    <slot>
-      <IconCheck
-        class="c-checkbox-icon"
-        :class="{
-          'c-checkbox-icon-on': !!modelValue,
-          'c-checkbox-icon-off': !modelValue,
-          'c-checkbox-icon-disabled': disabled,
-          'c-checkbox-icon-enabled': !disabled
-        }"
-      />
-    </slot>
-  </span>
-</template>
-
-<style lang="scss">
-
-// NOTE: this could also be confined in component class name
-// But then overriding it will be more laborious
-:root {
-  --c-checkbox-width: 1em;
-  --c-checkbox-border-width: 2px;
-  --c-checkbox-radius: var(--round);
-
-  --c-checkbox-enabled-on-color: var(--inverted-offset-background-color);
-  --c-checkbox-enabled-off-color: var(--text-color);
-  --c-checkbox-enabled-check-color: var(--inverted-text-color);
-
-  --c-checkbox-disabled-on-color: var(--discreet-color-very-light);
-  --c-checkbox-disabled-off-color: var(--discreet-text-color);
-  --c-checkbox-disabled-check-color: var(--discreet-text-color);
-}
-
-.c-checkbox,
-.c-checkbox-icon {
-  @include relative;
-  @include transition-leave;
-  @include transition-properties-common;
-}
-
-.c-checkbox {
-  @include inline-block;
-  vertical-align: middle;
-
-  width: var(--c-checkbox-width);
-  height: var(--c-checkbox-width);
-  border-width: var(--c-checkbox-border-width);
-  padding: calc(var(--pad-tight-horizontal) - (2 * var(--c-checkbox-border-width)));
-  border-radius: var(--c-checkbox-radius);
-
-  // Default for enabled, off state
-  color: var(--c-checkbox-enabled-check-color);
-  border-color: var(--c-checkbox-enabled-off-color);
-}
-
-.c-checkbox-icon {
-  @include block;
-  width: 1em;
-  height: 1em;
-
-  // Default for off state
-  opacity: 0;
-}
-
-
-
-// On states
-
-.c-checkbox-on,
-.c-checkbox-icon-on {
-  @include transition-enter;
-}
-
-.c-checkbox-on {
-  background-color: var(--c-checkbox-enabled-on-color);
-}
-
-.c-checkbox-icon-on {
-  opacity: 1;
-}
-
-
-
-// Enabled/disabled states
-
-.c-checkbox-disabled {
-  color: var(--c-checkbox-disabled-check-color);
-  border-color: var(--c-checkbox-disabled-off-color);
-  background-color: var(--c-checkbox-disabled-on-color);
-}
-
-// .c-checkbox-enabled {
-//   &.c-checkbox-on {}
-//   &.c-checkbox-off {}
-// }
-
-.control-enabled {
-  @include pointer;
-
-  .c-checkbox-enabled {
-    @include transparent-solid-shadow;
+  .c-checkbox,
+  .c-checkbox-icon {
+    @include relative;
+    @include transition-leave;
+    @include transition-properties-common;
   }
 
-  &:focus,
-  &:hover {
+  .c-checkbox {
+    @include inline-block;
+    vertical-align: middle;
+
+    width: var(--c-checkbox-width);
+    height: var(--c-checkbox-width);
+    border-width: var(--c-checkbox-border-width);
+    padding: calc(var(--pad-tight-horizontal) - (2 * var(--c-checkbox-border-width)));
+    border-radius: var(--c-checkbox-radius);
+
+    // Default for enabled, off state
+    color: var(--c-checkbox-enabled-check-color);
+    border-color: var(--c-checkbox-enabled-off-color);
+  }
+
+  .c-checkbox-icon {
+    @include block;
+    width: 1em;
+    height: 1em;
+
+    // Default for off state
+    opacity: 0;
+  }
+
+
+
+  // On states
+
+  .c-checkbox-on,
+  .c-checkbox-icon-on {
+    @include transition-enter;
+  }
+
+  .c-checkbox-on {
+    background-color: var(--c-checkbox-enabled-on-color);
+  }
+
+  .c-checkbox-icon-on {
+    opacity: 1;
+  }
+
+
+
+  // Enabled/disabled states
+
+  .c-checkbox-disabled {
+    color: var(--c-checkbox-disabled-check-color);
+    border-color: var(--c-checkbox-disabled-off-color);
+    background-color: var(--c-checkbox-disabled-on-color);
+  }
+
+  // .c-checkbox-enabled {
+  //   &.c-checkbox-on {}
+  //   &.c-checkbox-off {}
+  // }
+
+  .control-enabled {
+    @include pointer;
+
     .c-checkbox-enabled {
-      @include transition-fast;
-      @include solid-shadow;
+      @include transparent-solid-shadow;
+    }
+
+    &:focus,
+    &:hover {
+      .c-checkbox-enabled {
+        @include transition-fast;
+        @include solid-shadow;
+      }
     }
   }
-}
 
-</style>
+  </style>
