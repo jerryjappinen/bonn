d735092a8007e0832ecd69a490a17f0c95d150f2
Jerry Jäppinen <eiskis@gmail.com>
2022-10-08 12:55:29 +0200

    Added Relative

---
+++ components/Relative.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  inline: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <component
+    :is="inline ? 'span' : 'div'"
+    class="c-relative"
+  ><slot /></component>
+</template>
+
+<style lang="scss">
+
+.c-relative {
+  @include relative;
+}
+
+</style>
