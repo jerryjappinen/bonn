938e6ebaf1d78c4e91c62528c157c25c4efd664d
Jerry Jäppinen <eiskis@gmail.com>
2022-10-22 14:10:51 +0200

    defineNuxtPlugin

--- nuxt/plugins/piniaPersist.js
+++ nuxt/plugins/piniaPersist.js
@@ -1,8 +1,8 @@
 import { createNuxtPersistedState } from 'pinia-plugin-persistedstate/nuxt'
 
-export default (optionsInput) => {
+export default defineNuxtPlugin((optionsInput) => {
   return (nuxtApp) => {
     // `useCookie` is auto imported in Nuxt
     nuxtApp.$pinia.use(createNuxtPersistedState(useCookie, optionsInput))
   }
-}
+})
