0e23a9a47414919aa13069c6bfbe46f0cb60166c
Jerry Jäppinen <eiskis@gmail.com>
2023-03-26 17:27:02 +0200

    Robots spec

--- nuxt/config/robots.js
+++ nuxt/config/robots.js
@@ -1,8 +1,26 @@
-export default (rules, configPath) => {
+import compact from 'lodash-es/compact'
+import flatten from 'lodash-es/flatten'
+import isString from 'lodash-es/isString'
+
+export default (...optionsInput) => {
+  const ruleInputs = compact(flatten(optionsInput))
+
+  let rules = []
+  let configPath = ''
+
+  // Accept configPath in a convenient way
+  if (isString(ruleInputs[0])) {
+    configPath = ruleInputs[0]
+
+  // Otherwise treat input as list of rule definitions
+  } else {
+    rules = ruleInputs
+  }
+
   return {
     modules: ['@nuxtjs/robots'],
     robots: {
-      rules: rules || [],
+      rules,
       configPath
     }
   }
--- spec/nuxt/config/robots.spec.js
+++ spec/nuxt/config/robots.spec.js
@@ -4,9 +4,43 @@ import isEmpty from 'lodash-es/isEmpty'
 
 import robots from '../../../nuxt/config/robots'
 
+const path = 'path/to/robots/conf.js'
+
+// https://nuxt.com/modules/robots#configpath
+const rule1 = { UserAgent: '*' }
+const rule2 = { Disallow: '/' }
+const rule3 = { BlankLine: true }
+const rule4 = { Comment: 'Comment here' }
+const rule5 = {
+  UserAgent: '*',
+  Disallow: ''
+}
+
 describe.concurrent('robots config helper', () => {
-  it('should include no routes', () => {
+  it('should include no rules by default', () => {
     const conf = robots()
-    expect(isEmpty(conf.robots.routes)).toBeTruthy()
+    expect(isEmpty(conf.robots.rules)).toBeTruthy()
+  })
+
+  it('should accept config path', () => {
+    const conf = robots(path)
+
+    expect(conf.robots.configPath).toEqual(path)
+    expect(isEmpty(conf.robots.rules)).toBeTruthy()
+  })
+
+  it('should ignore rules when using configPath', () => {
+    const conf = robots(path, rule1, rule2)
+    expect(isEmpty(conf.robots.rules)).toBeTruthy()
+  })
+
+  it('should accept individual rule', () => {
+    const conf = robots(rule5)
+    expect(conf.robots.rules).toEqual([rule5])
+  })
+
+  it('should accept multiple rules', () => {
+    const conf = robots(rule1, rule2, rule3, rule4)
+    expect(conf.robots.rules).toEqual([rule1, rule2, rule3, rule4])
   })
 })
